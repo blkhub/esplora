51f4d0ccc41cb54e60c27c6735537aa5daf3d3b5
Antoine FERRON <anferron@gmail.com>
2019-08-04 18:21:35 +0200

    Site base URL customizable

--- client/src/views/navbar.js
+++ client/src/views/navbar.js
@@ -4,7 +4,7 @@ import menu from './navbar-menu'
 export default S =>
   <nav className="navbar navbar-dark navbar-expand-lg">
     <div className="container">
-      <a className="navbar-brand" href="."></a>
+      <a className="navbar-brand" href={process.env.SITE_BASE}></a>
 
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-menu" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
